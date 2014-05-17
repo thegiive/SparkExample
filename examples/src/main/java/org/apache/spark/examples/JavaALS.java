9c231f006c4faf735d223392e2c50fc2ac2eae09
Wisely Chen <thegiive@gmail.com>
2014-05-17 18:35:31 +0800

    Add find best model iteration and find top10

--- examples/src/main/java/org/apache/spark/examples/JavaALS.java
+++ examples/src/main/java/org/apache/spark/examples/JavaALS.java
@@ -2,6 +2,7 @@ package org.apache.spark.examples;
 
 import breeze.linalg.product;
 import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.*;
 import org.apache.spark.mllib.recommendation.*;
 import org.apache.spark.rdd.RDD;
@@ -33,6 +34,16 @@ public class JavaALS {
 
     }
 
+    public static List findTop10(MatrixFactorizationModel model, JavaSparkContext sc , int user_id , int total_product){
+        List<Tuple2<Integer , Integer>> arr = new ArrayList<>();
+        for(int i = 1 ; i <= total_product ; i++){
+            arr.add( new Tuple2( user_id, i));
+        }
+        JavaRDD testdata = sc.parallelize(arr);
+        JavaRDD<Rating> prediction = model.predict(testdata.rdd()).toJavaRDD();
+        return prediction.mapToPair(p -> new Tuple2(p.rating() , p.product() )).sortByKey(false).take(10) ;
+    }
+
 
     public static void main(String[] args) {
 
@@ -52,23 +63,52 @@ public class JavaALS {
         });
 
 
-        int numPartitions = 50;
+        int rank = Integer.parseInt(args[1]) ;
+        double lambda = 10.0 ;
+//        int numIter = Integer.parseInt(args[2]) ;
+        int numPartitions = Integer.parseInt(args[2]) ;
 
         JavaRDD<Rating> training = ratings.filter( x -> x._1() < 6).values().repartition(numPartitions).cache();
         JavaRDD<Rating> validation = ratings.filter( x -> x._1() >= 6 && x._1() < 8).values().repartition(numPartitions).cache();
         JavaRDD<Rating> test = ratings.filter( x -> x._1() >= 8).values().cache();
 
-        int rank = Integer.parseInt(args[1]) ;
-        double lambda = 10.0 ;
-        int numIter = Integer.parseInt( args[2] ) ;
+        System.out.println("Start Iteration");
 
-        System.out.println("Ranks is " + rank + " and inter is " + numIter);
-        MatrixFactorizationModel model = ALS.train(training.rdd(), rank, numIter, lambda);
+        Double minRmse = 100.0;
+        int bestNumIter = 0 ;
 
-        Double validationRmse = computeRmse(model, validation, validation.count());
-        System.out.println("RMSE (validation) = " + validationRmse + " for the model trained with rank = "
-                + rank + ", lambda = " + lambda + ", and numIter = " + numIter + ".");
+//        for( int numIter = 3 ; numIter <= 30 ; numIter ++ ) {
+//            MatrixFactorizationModel model = ALS.train(training.rdd(), rank, numIter, lambda);
+//            Double validationRmse = computeRmse(model, validation, validation.count());
+//            System.out.println("RMSE (validation) = " + validationRmse + " for the model trained with rank = "
+//                    + rank + ", lambda = " + lambda + ", and numIter = " + numIter + ".");
+//            if( minRmse < validationRmse ){
+//                System.out.println("Stop!!!");
+//                System.out.println("RMSE (validation) = " + validationRmse + " previous RMSE is "+minRmse+"for the model trained with rank = "
+//                        + rank + ", lambda = " + lambda + ", and numIter = " + numIter + ".");
+//                bestNumIter = numIter - 1 ;
+//
+//                break ;
+//            }else{
+//                minRmse = validationRmse;
+//            }
+//        }
 
+        MatrixFactorizationModel bestModel = ALS.train(training.rdd(), rank, 8 , lambda);
+        List result = findTop10(bestModel , sc , 6040 , 3952 );
+
+        System.out.println(result);
+        System.out.println(findTop10(bestModel , sc , 6039 , 3952 ));
+//        List<Tuple2<Integer , Integer>> arr = new ArrayList<>();
+//        for(int i = 1 ; i <= 3952 ; i++){
+//            arr.add( new Tuple2(6040 , i));
+//        }
+//        JavaRDD testdata = sc.parallelize(arr);
+//        JavaRDD<Rating> prediction = bestModel.predict(testdata.rdd()).toJavaRDD();
+//        System.out.println(prediction.mapToPair(p -> new Tuple2(p.rating() , p.product() )).sortByKey(false).take(10));
+//        System.out.println(prediction.take(10));
+
+        sc.stop();
 
     }
 }
