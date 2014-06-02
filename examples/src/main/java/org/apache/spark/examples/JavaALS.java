854abdd064a806bf6979058003b823ce4b684f47
Wisely Chen <thegiive@gmail.com>
2014-06-02 20:53:19 +0800

    Refactor the ALS code

--- examples/src/main/java/org/apache/spark/examples/JavaALS.java
+++ examples/src/main/java/org/apache/spark/examples/JavaALS.java
@@ -4,8 +4,12 @@ import breeze.linalg.product;
 import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.*;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.broadcast.Broadcast;
 import org.apache.spark.mllib.recommendation.*;
 import org.apache.spark.rdd.RDD;
+import org.jblas.DoubleMatrix;
+import scala.Array;
 import scala.Tuple2;
 import scala.tools.nsc.transform.patmat.MatchAnalysis;
 
@@ -13,36 +17,29 @@ import java.util.*;
 
 public class JavaALS {
 
-    /** Compute RMSE (Root Mean Squared Error). */
-    public static double computeRmse(MatrixFactorizationModel model , JavaRDD<Rating> data, Long n){
-
-        RDD userProduct = data.mapToPair(x -> new Tuple2(x.user(), x.product()) ).rdd();
-        JavaRDD<Rating> predictions  = model.predict( userProduct ).toJavaRDD() ;
-
-        JavaRDD<Tuple2<Double,Double>> predictionsAndRatings =
-            predictions.mapToPair( x -> new Tuple2( new Tuple2(x.user() , x.product()) , x.rating() ) )
-            .join(data.mapToPair(x -> new Tuple2( new Tuple2(x.user() , x.product()) , x.rating() ) ))
-            .values();
-
-        Double msr = predictionsAndRatings.mapToDouble(
-                        x -> { double predict = x._1() , real = x._2() ;
-                               return (( predict - real ) * ( predict - real)) ;
-                        }).sum() / n;
-
-        return Math.sqrt(msr);
-
+    public static RDD getModel(JavaSparkContext sc , String model_file_name){
+        JavaRDD<Tuple2<Integer,String>> rdd1 = sc.objectFile(model_file_name);
+        JavaPairRDD<Object , double[]> rdd2 = rdd1.mapToPair(s -> {
+            Integer user_id = s._1();
+            String[] arr = s._2().split(",");
+            double[] result = new double[arr.length];
+            for( int i = 0 ; i < arr.length ; i++ ) {
+                result[i] = Double.parseDouble(arr[i]) ;
+            }
+            return new Tuple2(user_id , result);
+        });
+        return rdd2.rdd() ;
     }
 
-    public static List findTop10(MatrixFactorizationModel model, JavaSparkContext sc , int user_id , int total_product){
-        List<Tuple2<Integer , Integer>> arr = new ArrayList<>();
-        for(int i = 1 ; i <= total_product ; i++){
-            arr.add( new Tuple2( user_id, i));
-        }
-        JavaRDD testdata = sc.parallelize(arr);
-        JavaRDD<Rating> prediction = model.predict(testdata.rdd()).toJavaRDD();
-        return prediction.mapToPair(p -> new Tuple2(p.rating() , p.product() )).sortByKey(false).take(10) ;
+    public static void writeRDDtoFile(JavaRDD<Tuple2<Object, double[]>> rdd , String file_name){
+        rdd.mapToPair(s -> {
+            Integer user_id = (Integer) s._1();
+            double[] vector = s._2();
+            String result = "";
+            for (double d : vector) result += d + ",";
+            return new Tuple2(user_id, result);
+        }).saveAsObjectFile(file_name);
     }
-
 
     public static void main(String[] args) {
 
@@ -60,158 +57,154 @@ public class JavaALS {
         int numPartitions = Integer.parseInt(args[4]) ;
         String outputFilePrefix = args[5];
 
+        MatrixFactorizationModel model =
+           new MatrixFactorizationModel(rank , getModel(sc,outputFilePrefix+"user.txt") , getModel(sc,outputFilePrefix+"product.txt")) ;
 
-        JavaPairRDD<Long, Rating> ratings = lines.mapToPair(line -> {
-            String[] arr = line.split(",");
-            String user = arr[0] , product = arr[1] , rating = arr[2] , time = arr[3];
-            Rating tmprat = new Rating(Integer.parseInt(user), Integer.parseInt(product), Double.parseDouble(rating));
-            return new Tuple2( Long.parseLong(time) % 10, tmprat);
-        });
+        ArrayList<Integer> product_arr = (ArrayList) lines.map(l ->{
+            String[] arr = l.split(",");
+            return Integer.parseInt(arr[1]);
+        }).distinct().collect();
 
+        Broadcast bb = sc.broadcast(product_arr) ;
 
+//        JavaRDD<Integer> users_rdd  = lines.map(l ->{
+//            String[] arr = l.split(",");
+//            return Integer.parseInt(arr[0]);
+//        }).distinct().cache();
 
-        // Seperate the data
-        JavaRDD<Rating> training = ratings.filter( x -> x._1() < 6).values().repartition(numPartitions).cache();
-        JavaRDD<Rating> validation = ratings.filter( x -> x._1() >= 6 && x._1() < 8).values().repartition(numPartitions).cache();
-        JavaRDD<Rating> test = ratings.filter( x -> x._1() >= 8).values();
-        JavaPairRDD<Integer,ArrayList<Integer>> testr = test.mapToPair( r -> new Tuple2(r.user() , r.product() )).groupByKey().cache();
+        JavaPairRDD<Integer, double[]> product_feature = model.productFeatures().toJavaRDD().mapToPair(t -> new Tuple2(t._1(), t._2())).cache();
+        JavaPairRDD<Integer, double[]> user_feature = model.userFeatures().toJavaRDD().mapToPair(t -> new Tuple2(t._1(), t._2())).cache();
 
-//        System.out.println("Start Iteration");
-//
-//        Double minRmse = 100.0;
-//        int bestNumIter = 0 ;
-//
-//        for( int numIter = 3 ; numIter <= 30 ; numIter ++ ) {
-//            MatrixFactorizationModel model = ALS.train(training.rdd(), rank, numIter, lambda);
-//            Double validationRmse = computeRmse(model, validation, validation.count());
-//            System.out.println("RMSE (validation) = " + validationRmse + " for the model trained with rank = "
-//                    + rank + ", lambda = " + lambda + ", and numIter = " + numIter + ".");
-//            if( minRmse < validationRmse ){
-//                System.out.println("Stop!!!");
-//                System.out.println("RMSE (validation) = " + validationRmse + " previous RMSE is "+minRmse+"for the model trained with rank = "
-//                        + rank + ", lambda = " + lambda + ", and numIter = " + numIter + ".");
-//                bestNumIter = numIter - 1 ;
-//
-//                break ;
-//            }else{
-//                minRmse = validationRmse;
-//            }
-//        }
+        HashMap<Integer , double[]> hm = new HashMap();
+        product_feature.collect().forEach(p -> hm.put(p._1() , p._2() ));
+        Broadcast bbb = sc.broadcast(hm);
+        for(int i = 0 ; i < 10 ; i ++ ) {
+            final int start = 500000 + (i*10000 );
+            final int end = 500000 + ((i+1)*10000 );
 
-//        System.out.println("Best Setting is iter="+bestNumIter+" lambda="+lambda);
+            JavaPairRDD<Integer, double[]> users_part_rdd = user_feature.filter(t -> t._1() >= start && t._1() < end).repartition(numPartitions);
 
+            JavaRDD<Rating> predict = users_part_rdd.map(t -> {
+                Integer user = t._1();
+                double[] uFeatures = t._2();
+                HashMap<Integer, double[]> tmp_hm = (HashMap) bbb.value();
+                double max_r = 0;
+                Integer max_p = 0;
 
-        MatrixFactorizationModel bestModel = ALS.train(training.rdd(), rank, iter , lambda);
-//        bestModel.userFeatures().toJavaRDD().mapToPair(s -> {
-//            Integer user_id = (Integer)s._1(); double[] feature_array = s._2(); int i = 1 ;
-//            ArrayList<Tuple2<Integer,Double>> result = new ArrayList();
-//            for( double f : feature_array) result.add(new Tuple2(i++,f));
-//            result.sort( (a, b) -> a._2() >= b._2() ? -1 : 1 );
-//            return new Tuple2( user_id , result.subList(0,4));
-//        }).saveAsTextFile(outputFilePrefix+"/user_feature.txt");
+                for (Integer product : (ArrayList<Integer>) bb.value()) {
+                    double r = 0.0;
+                    try {
+                        double[] pFeatures = tmp_hm.get(product);
+                        DoubleMatrix userVector = new DoubleMatrix(uFeatures);
+                        DoubleMatrix productVector = new DoubleMatrix(pFeatures);
+                        r = userVector.dot(productVector);
+                        if (r > max_r) {
+                            max_r = r;
+                            max_p = product;
+                        }
+                    } catch (Exception e) {
+//                    System.err.println("###"+product);
+                    }
+                }
+                return new Rating(user, max_p, max_r);
+            });
 
-        JavaPairRDD<Integer, Tuple2<Double,Integer>> pf = bestModel.productFeatures().toJavaRDD().flatMapToPair(s->{
-            Integer product_id = (Integer)s._1(); double[] feature_array = s._2(); int feature_id = 1 ;
-            List result = new ArrayList();
-            for( double f : feature_array) {
-                result.add(new Tuple2( feature_id++ , new Tuple2(f, product_id) ));
-            }
-            return result ;
-        });//.saveAsTextFile(outputFilePrefix+"/product_feature.txt");;
-//        pf.groupByKey().
-        JavaPairRDD<Integer , ArrayList<Tuple2>> ppf =  pf.groupByKey().mapValues(v -> {
-            ArrayList<Tuple2<Double, Integer>> result = new ArrayList();
-            v.forEach(t -> result.add(t));
-            result.sort((a,b) -> a._1() >= b._1() ? -1 : 1 );
-            return new ArrayList<Tuple2>(result.subList(0, 4));
-        });//.saveAsTextFile(outputFilePrefix+"/product_feature.txt");
+            System.out.println(predict.count());
+//            System.out.println(predict.take(10));
 
-        HashMap<Integer, String> hm = new HashMap() ;
-        List<Tuple2<Integer , ArrayList<Tuple2>>> ppf_list = ppf.collect();
-        for( Tuple2 t : ppf_list ){
-            String result ="";
-              for( Tuple2 tt : (ArrayList<Tuple2>) t._2()){
-                result += tt._1()+","+tt._2()+"#";
-              }
-            hm.put((Integer)t._1() , result );
         }
 
-        JavaPairRDD<Integer, ArrayList<Tuple2>> uf = bestModel.userFeatures().toJavaRDD().mapToPair(s -> {
-            Integer user_id = (Integer)s._1(); double[] feature_array = s._2(); int i = 1 ;
-            ArrayList<Tuple2<Integer,Double>> result = new ArrayList();
-            for( double f : feature_array) result.add(new Tuple2(i++,f));
-            result.sort( (a, b) -> a._2() >= b._2() ? -1 : 1 );
-            return new Tuple2( user_id , new ArrayList(result.subList(0,4)));
-        });//.saveAsTextFile(outputFilePrefix+"/user_feature.txt");
-
-        JavaPairRDD<Integer, ArrayList<Integer>> up = uf.mapValues( s -> {
-            ArrayList<Tuple2<Integer,Double>> tresult = new ArrayList();
-            s.forEach( t -> {
-                int feature_id = (int)t._1() ;
-                double feature_params = (double)t._2();
-
-                String r = hm.get( feature_id );
-                for( String r1 : r.split("#")) {
-                    String[] tarr = r1.split(",") ;
-                    double product_feature_params = Double.parseDouble(tarr[0]);
-                    int product_id = Integer.parseInt(tarr[1]);
-                    tresult.add( new Tuple2(product_id, product_feature_params*feature_params));
-                }
-            });
-            tresult.sort((a,b) -> a._2() >= b._2() ? -1 : 1 );
-            ArrayList<Integer> result = new ArrayList();
-            for( Tuple2<Integer,Double> t : tresult){
-                result.add(t._1());
-            }
-            return result ;
-        });
-
-        up.saveAsTextFile(outputFilePrefix+"/model.txt");
-
-        JavaRDD<Integer> final_predict_result = testr.join(up).flatMap(t -> {
-            Integer user_id = t._1();
-            ArrayList<Integer> tmp_result = new ArrayList(t._2()._1()) ;
-            ArrayList<Integer> tmp_predict = new ArrayList(t._2()._2()) ;
-            ArrayList<Integer> result = new ArrayList();
-            for (Integer r : tmp_result) {
-                if( tmp_predict.contains(r) ){
-                    result.add(1);
-                }else{
-                    result.add(0);
-                }
-            }
-            return result ;
-        });
+//            JavaPairRDD<Integer, Object> user_product = users_part_rdd.flatMapToPair(user -> {
+//                ArrayList<Tuple2<Integer, Object>> result = new ArrayList();
+//                ArrayList<Integer> cc = (ArrayList<Integer>) bb.value();
+//                for (Integer p : cc) {
+//                    result.add(new Tuple2(user, p));
+//                }
+//                return result;
+//            });
 
 
-        System.out.println(final_predict_result.filter(i -> i == 1).count());
-        System.out.println(final_predict_result.count());
+//        val users = userFeatures.join(usersProducts).map{
+//            case (user, (uFeatures, product)) => (product, (user, uFeatures))
+//        }
+//        users.join(productFeatures).map {
+//            case (product, ((user, uFeatures), pFeatures)) =>
+//                val userVector = new DoubleMatrix(uFeatures)
+//                val productVector = new DoubleMatrix(pFeatures)
+//                Rating(user, product, userVector.dot(productVector))
+//        }
 
-//        System.out.println(testr.join(up).take(5));
 
-//        bestModel.productFeatures().toJavaRDD().mapToPair(s -> new Tuple2( s._1() , Arrays.toString(s._2()) ) ).saveAsTextFile(outputFilePrefix+"/product_feature.txt");
-
-//        for( Tuple2<Object , double[]> t : bestModel.userFeatures().toJavaRDD().collect() ){
-//            if(((Integer)t._1()) == 5000) {
-//                System.out.println(t._1());
-//                Arrays.sort(t._2());
-//                System.out.println(Arrays.toString( t._2() ));
+//        JavaRDD<Rating> predict = user_product.join(user_feature).map(t -> {
+//           Integer user = t._1() ;
+//            double[] uFeatures = t._2()._2();
+//            Integer product = (Integer)t._2()._1();
+//            HashMap<Integer, double[]> tmp_hm = (HashMap) bbb.value() ;
+//            DoubleMatrix userVector = new DoubleMatrix(uFeatures);
+//            double r = 0.0;
+//            try {
+//                DoubleMatrix productVector = new DoubleMatrix(tmp_hm.get(product));
+//                r = userVector.dot(productVector);
+//            }catch(Exception e){
+//                System.err.println("###"+product);
 //            }
-//        }
-//        List result = findTop10(bestModel , sc , 6040 , 3952 );
+//            return new Rating(user, product, r);
+//
+////            return new Rating(user, product, (double)tmp_hm.get(product)[0] );
+//        });
 
-//        System.out.println(result);
-//        System.out.println(findTop10(bestModel , sc , 6039 , 3952 ));
-//        List<Tuple2<Integer , Integer>> arr = new ArrayList<>();
-//        for(int i = 1 ; i <= 3952 ; i++){
-//            arr.add( new Tuple2(6040 , i));
-//        }
-//        JavaRDD testdata = sc.parallelize(arr);
-//        JavaRDD<Rating> prediction = bestModel.predict(testdata.rdd()).toJavaRDD();
-//        System.out.println(prediction.mapToPair(p -> new Tuple2(p.rating() , p.product() )).sortByKey(false).take(10));
-//        System.out.println(prediction.take(10));
+//        System.out.println("#####"+Arrays.toString(product_feature.lookup(5072402).get(0)));
+
+
+//            JavaPairRDD<Integer, Tuple2<Integer, double[]>> tmp_users_rdd = user_product.join(user_feature).mapToPair(t -> {
+//                Integer user = t._1();
+//                double[] uFeatures = t._2()._2();
+//                Integer product = (Integer) t._2()._1();
+//                return new Tuple2(product, new Tuple2(user, uFeatures));
+//            });
+//
+//            JavaRDD<Rating> predict = tmp_users_rdd.join(product_feature).map(t -> {
+//                Integer product = t._1();
+//                Integer user = t._2()._1()._1();
+//                double[] uFeatures = t._2()._1()._2();
+//                double[] pFeatures = t._2()._2();
+//                DoubleMatrix userVector = new DoubleMatrix(uFeatures);
+//                DoubleMatrix productVector = new DoubleMatrix(pFeatures);
+//                return new Rating(user, product, userVector.dot(productVector));
+//            });
+//
+//        JavaRDD<Rating> result = model.predict(user_product.rdd()).toJavaRDD();
+//
+
+
+
+
+        // Load model from text file
+//        MatrixFactorizationModel model =
+//           new MatrixFactorizationModel(rank , getModel(sc,outputFilePrefix+"user.txt") , getModel(sc,outputFilePrefix+"product.txt")) ;
+//        System.out.println(model.predict(1 ,5072402));
+//        System.out.println(model.predict(2,4129220));
+
+
+//        // Save mode as text file
+//        JavaPairRDD<Long, Rating> ratings = lines.mapToPair(line -> {
+//            String[] arr = line.split(",");
+//            String user = arr[0] , product = arr[1] , rating = arr[2] , time = arr[3];
+//            Rating tmprat = new Rating(Integer.parseInt(user), Integer.parseInt(product), Double.parseDouble(rating));
+//            return new Tuple2( Long.parseLong(time) % 10, tmprat);
+//        });
+//
+//
+//        JavaRDD<Rating> training = ratings.filter( x -> x._1() < 8).values().repartition(numPartitions).cache();
+//
+//        MatrixFactorizationModel model = ALS.train(training.rdd(), rank, iter , lambda);
+//        writeRDDtoFile(model.userFeatures().toJavaRDD() , outputFilePrefix+"user.txt");
+//        writeRDDtoFile(model.productFeatures().toJavaRDD() , outputFilePrefix+"product.txt");
+//        // Save mode as text file END
+
+
         sc.stop();
 
     }
-        ;
-    }
+
+}
