6c8cc513567cdd733b6968a7982155f2a80eb97d
Wisely Chen <thegiive@gmail.com>
2014-06-03 17:05:29 +0800

    add ALS prediction

--- examples/src/main/java/org/apache/spark/examples/JavaALS.java
+++ examples/src/main/java/org/apache/spark/examples/JavaALS.java
@@ -1,6 +1,7 @@
 package org.apache.spark.examples;
 
 import breeze.linalg.product;
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.*;
@@ -41,6 +42,21 @@ public class JavaALS {
         }).saveAsObjectFile(file_name);
     }
 
+    public static Rating[] sort(Rating[] arr , Rating input){
+
+        double min_value = arr[0].rating() ; int min_index = 0 ;
+        for( int i = 1 ; i < arr.length ; i++){
+            if(arr[i].rating() < min_value){
+                min_value = arr[i].rating() ;
+                min_index = i ;
+            }
+        }
+        if( min_value <= input.rating()){
+            arr[min_index] = input;
+        }
+        return arr;
+    }
+
     public static void main(String[] args) {
 
         if (args.length < 6) {
@@ -67,29 +83,26 @@ public class JavaALS {
 
         Broadcast bb = sc.broadcast(product_arr) ;
 
-//        JavaRDD<Integer> users_rdd  = lines.map(l ->{
-//            String[] arr = l.split(",");
-//            return Integer.parseInt(arr[0]);
-//        }).distinct().cache();
-
         JavaPairRDD<Integer, double[]> product_feature = model.productFeatures().toJavaRDD().mapToPair(t -> new Tuple2(t._1(), t._2())).cache();
         JavaPairRDD<Integer, double[]> user_feature = model.userFeatures().toJavaRDD().mapToPair(t -> new Tuple2(t._1(), t._2())).cache();
 
         HashMap<Integer , double[]> hm = new HashMap();
         product_feature.collect().forEach(p -> hm.put(p._1() , p._2() ));
         Broadcast bbb = sc.broadcast(hm);
-        for(int i = 0 ; i < 10 ; i ++ ) {
-            final int start = 500000 + (i*10000 );
-            final int end = 500000 + ((i+1)*10000 );
+        for(int i = 0 ; i < 1 ; i ++ ) {
+            final int start = 500000 + (i*1000 );
+            final int end = 500000 + ((i+1)*1000 );
 
             JavaPairRDD<Integer, double[]> users_part_rdd = user_feature.filter(t -> t._1() >= start && t._1() < end).repartition(numPartitions);
 
-            JavaRDD<Rating> predict = users_part_rdd.map(t -> {
+            JavaPairRDD<Integer , String> predict = users_part_rdd.mapToPair(t -> {
                 Integer user = t._1();
                 double[] uFeatures = t._2();
                 HashMap<Integer, double[]> tmp_hm = (HashMap) bbb.value();
-                double max_r = 0;
-                Integer max_p = 0;
+
+                Rating[] tarr = new Rating[5];
+                for(int j = 0 ; j <5 ; j++) tarr[j] = new Rating(user , 0 , 0) ;
+
 
                 for (Integer product : (ArrayList<Integer>) bb.value()) {
                     double r = 0.0;
@@ -98,83 +111,24 @@ public class JavaALS {
                         DoubleMatrix userVector = new DoubleMatrix(uFeatures);
                         DoubleMatrix productVector = new DoubleMatrix(pFeatures);
                         r = userVector.dot(productVector);
-                        if (r > max_r) {
-                            max_r = r;
-                            max_p = product;
-                        }
+
+                        tarr = sort(tarr, new Rating(user , product , r));
+
                     } catch (Exception e) {
 //                    System.err.println("###"+product);
                     }
                 }
-                return new Rating(user, max_p, max_r);
+
+
+                ArrayList<Rating> tarr_list = new ArrayList(Arrays.asList(tarr));
+                tarr_list.sort((r1,r2) -> r1.rating() < r2.rating() ? 1 : -1 );
+                return new Tuple2(user , tarr_list.toString() );
             });
 
-            System.out.println(predict.count());
-//            System.out.println(predict.take(10));
-
+            System.out.println(predict.take(10));
+//            System.out.println(predict.count());
         }
 
-//            JavaPairRDD<Integer, Object> user_product = users_part_rdd.flatMapToPair(user -> {
-//                ArrayList<Tuple2<Integer, Object>> result = new ArrayList();
-//                ArrayList<Integer> cc = (ArrayList<Integer>) bb.value();
-//                for (Integer p : cc) {
-//                    result.add(new Tuple2(user, p));
-//                }
-//                return result;
-//            });
-
-
-//        val users = userFeatures.join(usersProducts).map{
-//            case (user, (uFeatures, product)) => (product, (user, uFeatures))
-//        }
-//        users.join(productFeatures).map {
-//            case (product, ((user, uFeatures), pFeatures)) =>
-//                val userVector = new DoubleMatrix(uFeatures)
-//                val productVector = new DoubleMatrix(pFeatures)
-//                Rating(user, product, userVector.dot(productVector))
-//        }
-
-
-//        JavaRDD<Rating> predict = user_product.join(user_feature).map(t -> {
-//           Integer user = t._1() ;
-//            double[] uFeatures = t._2()._2();
-//            Integer product = (Integer)t._2()._1();
-//            HashMap<Integer, double[]> tmp_hm = (HashMap) bbb.value() ;
-//            DoubleMatrix userVector = new DoubleMatrix(uFeatures);
-//            double r = 0.0;
-//            try {
-//                DoubleMatrix productVector = new DoubleMatrix(tmp_hm.get(product));
-//                r = userVector.dot(productVector);
-//            }catch(Exception e){
-//                System.err.println("###"+product);
-//            }
-//            return new Rating(user, product, r);
-//
-////            return new Rating(user, product, (double)tmp_hm.get(product)[0] );
-//        });
-
-//        System.out.println("#####"+Arrays.toString(product_feature.lookup(5072402).get(0)));
-
-
-//            JavaPairRDD<Integer, Tuple2<Integer, double[]>> tmp_users_rdd = user_product.join(user_feature).mapToPair(t -> {
-//                Integer user = t._1();
-//                double[] uFeatures = t._2()._2();
-//                Integer product = (Integer) t._2()._1();
-//                return new Tuple2(product, new Tuple2(user, uFeatures));
-//            });
-//
-//            JavaRDD<Rating> predict = tmp_users_rdd.join(product_feature).map(t -> {
-//                Integer product = t._1();
-//                Integer user = t._2()._1()._1();
-//                double[] uFeatures = t._2()._1()._2();
-//                double[] pFeatures = t._2()._2();
-//                DoubleMatrix userVector = new DoubleMatrix(uFeatures);
-//                DoubleMatrix productVector = new DoubleMatrix(pFeatures);
-//                return new Rating(user, product, userVector.dot(productVector));
-//            });
-//
-//        JavaRDD<Rating> result = model.predict(user_product.rdd()).toJavaRDD();
-//
 
 
 
