f9ddd05722bd3c7cf67b687c1b9397b33bf2d6d8
Wisely Chen <thegiive@gmail.com>
2014-10-04 00:02:34 +0800

    change to pyspark startup

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ cp nbpasswd.txt /root/.ipython/profile_default/nbpasswd.txt
 cp ipython/ipython_notebook_config.py /root/.ipython/profile_default/ipython_notebook_config.py
 cp ipython/00-pyspark-setup.py /root/.ipython/profile_default/startup/00-pyspark-setup.py
 echo 'https://'`ec2-metadata -p | awk -F" " '{print $2}'`':8888/'
-ipython notebook  --no-mathjax
+cd /root/spark ; IPYTHON_OPTS="notebook" ./bin/pyspark
