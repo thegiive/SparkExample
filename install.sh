92b6b94945ace18b40cf0c05cf4139c9a7e7b980
Wisely Chen <thegiive@gmail.com>
2014-10-04 00:07:26 +0800

    add aws key

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ cp nbpasswd.txt /root/.ipython/profile_default/nbpasswd.txt
 cp ipython/ipython_notebook_config.py /root/.ipython/profile_default/ipython_notebook_config.py
 cp ipython/00-pyspark-setup.py /root/.ipython/profile_default/startup/00-pyspark-setup.py
 echo 'https://'`ec2-metadata -p | awk -F" " '{print $2}'`':8888/'
+export AWS_ACCESS_KEY_ID=AKIAJVERJHTPLWB33UIA AWS_SECRET_ACCESS_KEY=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
 cd /root/spark ; IPYTHON_OPTS="notebook" ./bin/pyspark
