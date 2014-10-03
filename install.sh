e8daea0496c3dcc282434189c66adf8b7c2d387c
Wisely Chen <thegiive@gmail.com>
2014-10-03 16:16:02 +0800

    add

--- install.sh
+++ install.sh
@@ -1,1 +1,4 @@
-sudo apt-get install awscli
+cd /root; openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout mycert.pem -out mycert.pem
+ipython profile create default
+python -c "from IPython.lib import passwd; print passwd()" > /root/.ipython/profile_default/nbpasswd.txt
+
