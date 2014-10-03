c0a9c20bb2807fde0011657420a548587687e7f6
Wisely Chen <thegiive@gmail.com>
2014-10-03 17:50:37 +0800

    add

--- ipython_notebook_config.py
+++ ipython_notebook_config.py
@@ -1,6 +1,6 @@
 c = get_config()
 c.NotebookApp.certfile = u'/root/mycert.pem'
-c.NotebookApp.ip = 'ec2-54-169-73-32.ap-southeast-1.compute.amazonaws.com'
+c.NotebookApp.ip = '*'
 c.NotebookApp.open_browser = False
 c.NotebookApp.port = 8888
 PWDFILE="/root/.ipython/profile_default/nbpasswd.txt"
