e1f136c99773f3acbbebc01ecaad632615497ce9
Wisely Chen <thegiive@gmail.com>
2014-10-03 23:42:32 +0800

    add pysaprk

--- ipython/00-pyspark-setup.py
+++ ipython/00-pyspark-setup.py
@@ -3,3 +3,5 @@ os.environ['SPARK_HOME'] = '/root/spark/'
 import sys
 sys.path.insert(0, '/root/spark/python')
 CLUSTER_URL = open('/root/spark-ec2/cluster-url').read().strip()
+from pyspark import  SparkContext
+sc = SarkContext( CLUSTER_URL, 'pyspark')
