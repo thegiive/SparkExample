c3a816442db9f5f7d520bfbeb050c9a79e323301
Ubuntu <ubuntu@ip-172-31-10-98.ap-southeast-1.compute.internal>
2014-10-03 15:34:11 +0000

    add

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 cp mycert.pem /root
 ipython profile create default
 cp nbpasswd.txt /root/.ipython/profile_default/nbpasswd.txt
-cp ipython_notebook_config.py /root/.ipython/profile_default/ipython_notebook_config.py
-cp 00-pyspark-setup.py /root/.ipython/profile_default/startup/00-pyspark-setup.py
+cp ipython/ipython_notebook_config.py /root/.ipython/profile_default/ipython_notebook_config.py
+cp ipython/00-pyspark-setup.py /root/.ipython/profile_default/startup/00-pyspark-setup.py
 echo 'https://'`ec2-metadata -p | awk -F" " '{print $2}'`':8888/'
 ipython notebook
