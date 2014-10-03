9ccf25d8828233fc90c3b212d4438452998a138a
Wisely Chen <thegiive@gmail.com>
2014-10-03 23:21:04 +0800

    add public dns

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ ipython profile create default
 cp nbpasswd.txt /root/.ipython/profile_default/nbpasswd.txt
 cp ipython_notebook_config.py /root/.ipython/profile_default/ipython_notebook_config.py
 cp 00-pyspark-setup.py /root/.ipython/profile_default/startup/00-pyspark-setup.py
-echo 'https://'`hostname`':8888/'
+echo 'https://'`ec2-metadata -p | awk -F" " '{print $2}'`':8888/'
 ipython notebook
