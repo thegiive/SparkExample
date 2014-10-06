04c5fcd083f54987106445995bae6981cc38287a
Wisely Chen <thegiive@gmail.com>
2014-10-06 11:54:51 +0800

    add file

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ ipython profile create default
 cp nbpasswd.txt /root/.ipython/profile_default/nbpasswd.txt
 cp ipython/ipython_notebook_config.py /root/.ipython/profile_default/ipython_notebook_config.py
 cp ipython/00-pyspark-setup.py /root/.ipython/profile_default/startup/00-pyspark-setup.py
+yum install aws-cli
+aws s3 cp s3://thegiive/s3/story.txt ~/story.txt
+/root/ephemeral-hdfs/bin/hadoop fs -put ~/story.txt /
 echo 'https://'`ec2-metadata -p | awk -F" " '{print $2}'`':8888/'
 cp *.ipynb /root/spark
 cd /root/spark ; IPYTHON_OPTS="notebook" ./bin/pyspark
