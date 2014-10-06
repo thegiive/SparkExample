53ce4aea4014149368a44db3d0891e7e667c1106
Wisely Chen <thegiive@gmail.com>
2014-10-06 18:37:29 +0800

    remove key

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ ipython profile create default
 cp nbpasswd.txt /root/.ipython/profile_default/nbpasswd.txt
 cp ipython/ipython_notebook_config.py /root/.ipython/profile_default/ipython_notebook_config.py
 cp ipython/00-pyspark-setup.py /root/.ipython/profile_default/startup/00-pyspark-setup.py
-yum install aws-cli
 echo 'https://'`ec2-metadata -p | awk -F" " '{print $2}'`':8888/'
 cp *.ipynb /root/spark
 cd /root/spark ; IPYTHON_OPTS="notebook  --no-mathjax" ./bin/pyspark
